c37d65a5057706b158f1344b1dbdf3e179344649
Alex dos Santos <s2it_alsantos@uolinc.com>
2019-10-16 15:22:00 -0300

    Desabilitar Menu na tela de Login

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, IonicPage } from 'ionic-angular';
+import { MenuController } from 'ionic-angular/components/app/menu-controller';
 
 @IonicPage()
 @Component({
@@ -8,9 +9,20 @@ import { NavController, IonicPage } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public menu: MenuController) {
 
   }
+
+  // Desabilita o menu ao entrar tela de login
+  ionViewWillEnter(){
+    this.menu.swipeEnable(false);
+  }
+
+  // habilita o menu ao sair da tela de login
+  ionViewDidLeave(){
+    this.menu.swipeEnable(true);
+  }
+
 
   login() {
     this.navCtrl.setRoot('CategoriasPage');
