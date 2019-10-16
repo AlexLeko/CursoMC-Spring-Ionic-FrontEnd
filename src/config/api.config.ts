53da4e21d2db8a0887e0e6c10b60500bcba8c344
Alex dos Santos <s2it_alsantos@uolinc.com>
2019-10-16 18:55:39 -0300

    Listando categorias com Imagens da AWS S3

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,5 +1,6 @@
 export const API_CONFIG = {
 
-    baseUrl: "http://localhost:8080" 
+    baseUrl: "http://localhost:8080",
+    bucketBaseUrl: "https://curso-java-spring-nelioalves.s3-sa-east-1.amazonaws.com"
 
 }
